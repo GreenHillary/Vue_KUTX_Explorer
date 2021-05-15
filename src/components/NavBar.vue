bd828f4274968c3f8a8d60db016634eaf0080c0c
GreenHillary <hgreenatx@gmail.com>
2021-05-15 16:49:10 -0500

    Centered audio player, removed Discogs and Who Sampled links

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,15 +3,6 @@
   <b-navbar toggleable="lg" type="dark" variant="info">
     <b-navbar-brand href="#">KUTX</b-navbar-brand>
 
-  <div class="container">
-    <audio
-        controls
-        src="https://kut.streamguys1.com/kutx-free">
-            Your browser does not support the
-            <code>audio</code> element.
-    </audio>
-  </div>
-
   </b-navbar>
 </div>
 </template>
